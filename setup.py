2b6f1a838fec6ba612469b6d2713cda5f31da63e
Yanone <post@yanone.de>
2025-02-02 11:26:50 +0100

    Wheel in /web

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="supersetter",
     version="0.1.0",
+    package_dir={"": "Lib"},
     packages=find_packages(),
     install_requires=[
         # Add your dependencies here
