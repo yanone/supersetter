a44f35b7e20d3b181834c61fd96098ef98643013
Yanone <post@yanone.de>
2025-02-02 11:09:38 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="supersetter",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # Add your dependencies here
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
